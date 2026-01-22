989e50ba8dffb66a32e779a29855ac6d526eb303
Gorjian1 <123899744+Gorjian1@users.noreply.github.com>
2026-01-22 15:24:21 +0300

    Fix station aggregation across records

---
+++ Nivtropy.Tests/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using Nivtropy.Application.Services;
+using Nivtropy.Domain.Model;
+
+namespace Nivtropy.Tests;
+
+public class ImportValidationServiceTests
+{
+    [Fact]
+    public void StationCounter_CountsStationFromSingleRecord()
+    {
+        var counter = new ImportValidationService.StationCounter();
+        var record = new MeasurementRecord { Rb_m = 1.234, Rf_m = 1.111 };
+
+        counter.TryAddStation(record, out _);
+
+        Assert.Equal(1, counter.Count);
+    }
+
+    [Fact]
+    public void StationCounter_CountsStationAcrossRecordsInSameRun()
+    {
+        var counter = new ImportValidationService.StationCounter();
+        var backRecord = new MeasurementRecord { Rb_m = 1.234 };
+        var foreRecord = new MeasurementRecord { Rf_m = 1.111 };
+
+        counter.TryAddStation(backRecord, out _);
+        counter.TryAddStation(foreRecord, out _);
+
+        Assert.Equal(1, counter.Count);
+    }
+
+    [Fact]
+    public void StationCounter_DoesNotCombineAcrossRunBoundary()
+    {
+        var counter = new ImportValidationService.StationCounter();
+        var backRecord = new MeasurementRecord { Rb_m = 1.234 };
+        var foreRecord = new MeasurementRecord { Rf_m = 1.111 };
+
+        counter.TryAddStation(backRecord, out _);
+        counter.ResetPending();
+        counter.TryAddStation(foreRecord, out _);
+
+        Assert.Equal(0, counter.Count);
+    }
+}
